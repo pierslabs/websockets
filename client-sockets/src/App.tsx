953ea9f8cf1e0ae08357cb90d883c3c4b4e7f4c6
pierslabs <pedrolosasp@gmail.com>
2023-03-12 08:40:35 +0100

    Add styles client.

--- client-sockets/src/App.tsx
+++ client-sockets/src/App.tsx
@@ -4,22 +4,46 @@ import { useEffect } from 'react';
 import { useSocket } from './hooks/useSockets';
 
 function App() {
-  const { connectSocket } = useSocket();
-
+  const { connectSocket, online } = useSocket();
+  console.log(online);
   useEffect(() => {
     console.log('connected');
     connectSocket();
   }, [connectSocket]);
 
+  const messages = [
+    { name: 'pier', message: ' hola que tal?' },
+    { name: 'pasku', message: ' hola que tal?' },
+    { name: 'lilian', message: ' hola que tal?' },
+  ];
+
   return (
-    <div className="App">
+    <div className="container-fluid">
       <h1>Typescript sockets</h1>
-      <h3>
-        {/* <span style={{ color: isConnected === 'online' ? 'green' : 'red' }}>
-          {isConnected === 'online' ? 'online' : 'offline'}
-        </span> */}
-      </h3>
-      <h3>userID: </h3>
+      <hr />
+
+      <div className="row">
+        <div className="col-4">
+          <form className="card p-3">
+            <input type="text" placeholder="message" className="form-control mt-2 " />
+            <button className="btn btn-primary mt-4">Send</button>
+          </form>
+        </div>
+
+        {/*  */}
+        <div className="col-8">
+          <div className="container border border-primary p-4">
+            <div className="border-light">
+              {messages.map((msg) => (
+                <div className="container p-1 m-2 bg-light " key={msg.name}>
+                  <p className="text-primary text-start fw-light">{msg.name}</p>
+                  <p className="text-start m-2 text-dark ">{msg.message}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
